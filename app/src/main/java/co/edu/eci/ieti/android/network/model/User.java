971fe054f8350d9a0e48a1e00a447ee6f90f2cfb
JonatanGonzalez09 <jonatan0924@gmail.com>
2020-11-26 09:56:40 -0500

    Lab 13- IETI

---
+++ app/src/main/java/co/edu/eci/ieti/android/network/model/User.java
@@ -0,0 +1,69 @@
+package co.edu.eci.ieti.android.network.model;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class User {
+
+    @PrimaryKey
+    @NonNull
+    private String id;
+
+    private String email;
+    private String password;
+    private String firstName;
+    private String username;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/co/edu/eci/ieti/android/network/service/TaskService.java
@@ -0,0 +1,14 @@
+package co.edu.eci.ieti.android.network.service;
+
+import java.util.List;
+
+import co.edu.eci.ieti.android.network.model.Task;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+
+public interface TaskService{
+    @GET("api/task/user/{userId}")
+    Call<List<Task>> userTasks(@Path("userId") String userId);
+}
